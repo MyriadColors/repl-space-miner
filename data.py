90b5012fb85c4d4e2ce90f7b0f463b2734802a11
MyriadColors <pedrotorresgamedesign@gmail.com>
2024-09-01 16:16:14 -0300

    Created: data.py file for holding constants.

---
+++ data.py
@@ -0,0 +1,4 @@
+from ore import MagnetonOre, JonniteOre, HeronOre, CyclonOre, OxyniteOre, VariteOre, AngionOre, AscorbonOre, PyrogenOre
+
+ORES = [PyrogenOre(), AscorbonOre(), AngionOre(), VariteOre(), OxyniteOre(),
+            CyclonOre(), HeronOre(), JonniteOre(), MagnetonOre()]
