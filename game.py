ccb3ecfd83110401f1067ca0fe39f64e1e0c79a3
MyriadColors <pedrotorresgamedesign@gmail.com>
2024-09-03 00:50:10 -0300

    Update: Now the player ship starts at a random field.

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
+from random import choice
+
 from ship import Ship
 from solar_system import SolarSystem
-from vector2d import Vector2d
 
 
 class Game:
@@ -8,7 +9,8 @@ class Game:
     def __init__(self, ship_name: str):
         self.global_time: int = 0
         self.solar_system: SolarSystem = SolarSystem(200, 50)
-        self.player_ship: Ship = Ship(Vector2d(0, 0), 0.005, 100, 0.05, 100,
+        rnd_field = choice(self.solar_system.asteroid_fields)
+        self.player_ship: Ship = Ship(rnd_field.position, 0.005, 100, 0.05, 100,
                                       100, 1, ship_name)
         self.player_credits = 1000
 
