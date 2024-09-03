f6f21728dac502094c4fdf0aa4ba71c595d7b50e
MyriadColors <pedrotorresgamedesign@gmail.com>
2024-09-03 01:20:23 -0300

    Removed the song because I forgot to contact the original composer (my bad).

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from game import Game
 from helpers import take_input
 from repl import display_welcome_message, start_repl
-from pygame import mixer
 
 
 def main():
-    mixer.init()
-    mixer.music.load("snow.mp3")
-    mixer.music.play(-1)
     display_welcome_message()
     ship_name = take_input("Enter your ship name: ").strip()
     game = Game(ship_name)
