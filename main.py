7006a7c14808560c1851b43c7de31b12ec496783
MyriadColors <pedrotorresgamedesign@gmail.com>
2024-09-10 18:12:19 -0300

    New feature: command-line argument parser using the `argparse` module in Python. This allows users to run the script with an optional `--mute` flag to control whether the background music plays.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import argparse
 from src.repl import start_repl
 from pygame import mixer
 
-def main():
+def main(args_input):
     mixer.init()
     mixer.music.load("Decoherence.mp3")
-    mixer.music.play(-1)
+    if not args_input.mute:
+        mixer.music.play(-1)
     start_repl()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Space Trader CLI Game')
+    parser.add_argument('--mute', action='store_true', help='Mute the music')
+    args = parser.parse_args()
+    main(args)
