41f02b057e78ff6448db7e6bea8e89957546731a
MyriadColors <pedrotorresgamedesign@gmail.com>
2024-09-10 00:55:39 -0300

    refactor(ore): Refactor Ore classes and data structures
    - Simplifies Ore class implementation by using dataclasses.
    - Defines Ores using a dictionary for better organization and flexibility.
    - Changes OreCargo to include buy and sell prices.
    - Removes unused code.

--- src/classes/ore.py
+++ src/classes/ore.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
 class Ore:
-    def __init__(self, name, value, volume, mineral_yield, id: int):
-        self.name: str = name
-        self.base_value: float = value  # in credits
-        self.volume: float = volume  # in m³ per unit
-        self.id: int = id
+    name: str
+    base_value: float  # in credits
+    volume: float  # in m³ per unit
+    mineral_yield: list  # Placeholder for future use
+    id: int
 
     def to_string(self):
         return f"{self.name}: {self.base_value} credits, {self.volume} m³ per unit"
@@ -15,45 +18,15 @@ class Ore:
         return self.name.lower()
 
 
-class PyrogenOre(Ore):
-    def __init__(self):
-        super().__init__("Pyrogen", 29.0, 0.3, [], 0)
-
-
-class AscorbonOre(Ore):
-    def __init__(self):
-        super().__init__("Ascorbon", 16.0, 0.15, [], 1)
-
-
-class AngionOre(Ore):
-    def __init__(self):
-        super().__init__("Angion", 55.0, 0.35, [], 2)
-
-
-class VariteOre(Ore):
-    def __init__(self):
-        super().__init__("Varite", 18.0, 0.1, [], 3)
-
-
-class OxyniteOre(Ore):
-    def __init__(self):
-        super().__init__("Oxynite", 3500.0, 16, [], 4)
-
-class CyclonOre(Ore):
-    def __init__(self):
-        super().__init__("Cyclon", 600.0, 2, [], 5)
-
-
-class HeronOre(Ore):
-    def __init__(self):
-        super().__init__("Heron", 1200.0, 3, [], 6)
-
-
-class JonniteOre(Ore):
-    def __init__(self):
-        super().__init__("Jonnite", 7250.0, 16, [], 7)
-
-
-class MagnetonOre(Ore):
-    def __init__(self):
-        super().__init__("Magneton", 580.0, 1.2, [], 8)
+# Define ores in a dictionary
+ORES = {
+    0: Ore("Pyrogen", 29.0, 0.3, [], 0),
+    1: Ore("Ascorbon", 16.0, 0.15, [], 1),
+    2: Ore("Angion", 55.0, 0.35, [], 2),
+    3: Ore("Varite", 18.0, 0.1, [], 3),
+    4: Ore("Oxynite", 3500.0, 16, [], 4),
+    5: Ore("Cyclon", 600.0, 2, [], 5),
+    6: Ore("Heron", 1200.0, 3, [], 6),
+    7: Ore("Jonnite", 7250.0, 16, [], 7),
+    8: Ore("Magneton", 580.0, 1.2, [], 8),
+}
